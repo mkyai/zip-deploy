49d2e48fbbec97653a1e1ed1826a6696d6f20ced
lovneet <lovneet@radiansys.com>
2023-08-11 09:54:13 +0530

    optionals

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-'use strict'
+"use strict";
 
-const core = require('@actions/core')
-const { exec } = require('child_process')
-require('dotenv').config()
+const core = require("@actions/core");
+const { exec } = require("child_process");
+require("dotenv").config();
 
 const main = async () => {
-  const webhook = core.getInput('webhook') || process.env.DEPLOY_WEBHOOK
-  const secret = core.getInput('secret') || process.env.WEBHOOK_SECRET
-  const msg = core.getInput('message')
+  const webhook = core.getInput("webhook") || process.env.DEPLOY_WEBHOOK;
+  const secret = core.getInput("secret") || process.env.WEBHOOK_SECRET;
+  // const msg = core.getInput('message')
 
   exec(
     `zip -r code.zip . && curl -X POST -H "Authorization: ${secret}" -F "zipFile=@code.zip" ${webhook}`,
     (error, stdout) => {
       if (error) {
-        console.log('ERROR:', error)
-        throw new Error(error)
+        console.log("ERROR:", error);
+        throw new Error(error);
       }
-      console.log('STDOUT', stdout)
-      core.setOutput('response', stdout)
+      console.log("STDOUT", stdout);
+      core.setOutput("response", stdout);
     }
-  )
-}
+  );
+};
 
-main().catch((err) => core.setFailed(err.message))
+main().catch((err) => core.setFailed(err.message));
